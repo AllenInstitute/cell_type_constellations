794c21da5e00cbe5b7d33c5ac4cf84046e88756e
danielsf <scott.daniel@alleninstitute.org>
2025-05-14 09:29:03 -0700

    add utility to plot umap to a PNG

---
+++ src/cell_type_constellations/umap/plot_utils.py
@@ -0,0 +1,75 @@
+"""
+Scripts to create the UMAP scatter plot in temporary files
+"""
+
+import matplotlib.figure
+import numpy as np
+
+
+def plot_embedding(
+        cell_set,
+        embedding_coords,
+        fov,
+        discrete_color_map,
+        color_by,
+        dst_path):
+    """
+    Parameters
+    ----------
+    cell_set:
+        the CellSet containing the per-cell metadata
+    embedding_coords:
+        the (n_cells, 2) numpy array of embedding coordinates
+        (rows must be in the same order as the cells in cell_set)
+    fov:
+        the FieldOfView defining the visualization
+    discrete_color_map:
+        the dict mapping [type_field][type_value] to color
+    color_by:
+        the type_field we are coloring by
+        (if None, everything will be gray)
+    dst_path:
+        path to file where scatter plot will be saved
+    """
+    rng = np.random.default_rng(771812311)
+    gray = '#dddddd'
+    n_cells = embedding_coords.shape[0]
+    if color_by is None:
+        color_array = [gray]*n_cells
+    else:
+        type_value_array = cell_set.type_value_from_idx(
+            type_field=color_by,
+            idx_array=np.arange(n_cells, dtype=int)
+        )
+        color_array = [
+            discrete_color_map[color_by][value]
+            for value in type_value_array
+        ]
+    color_array = np.array(color_array)
+
+    embedding_coords = fov.transform_to_pixel_coordinates(
+        embedding_coords=embedding_coords
+    )
+
+    xx = embedding_coords[:, 0]
+    yy = embedding_coords[:, 1]
+
+    idx = np.arange(n_cells, dtype=int)
+    rng.shuffle(idx)
+    xx = xx[idx]
+    yy = yy[idx]
+    color_array = color_array[idx]
+    fig = matplotlib.figure.Figure(figsize=(20, 20*fov.height/fov.width))
+    axis = fig.add_subplot(1, 1, 1)
+    axis.scatter(
+        xx,
+        yy,
+        c=color_array,
+        s=1
+    )
+    axis.axis('off')
+    axis.set_xlim((0, fov.width))
+    axis.set_ylim((0, fov.height))
+    fig.tight_layout()
+    fig.savefig(dst_path, bbox_inches=0)
+    
