201f1d8ea3bbcdf860063fbb9cf22fdce847eda0
danielsf <scott.daniel@alleninstitute.org>
2025-05-14 11:35:19 -0700

    can store scatter plot PNGs in HDF5 file

--- src/cell_type_constellations/umap/plot_utils.py
+++ src/cell_type_constellations/umap/plot_utils.py
@@ -2,8 +2,10 @@
 Scripts to create the UMAP scatter plot in temporary files
 """
 
+import h5py
 import matplotlib.figure
 import numpy as np
+import pathlib
 import PIL.ImageColor
 
 from cell_type_mapper.utils.utils import mkstemp_clean
@@ -92,4 +94,66 @@ def plot_embedding(
     axis.set_ylim((0, fov.height))
     fig.tight_layout()
     fig.savefig(dst_path, bbox_inches=0)
-    
+
+
+def add_scatterplot_to_hdf5(
+        cell_set,
+        embedding_coords,
+        fov,
+        discrete_color_map,
+        hdf5_path,
+        tmp_dir):
+    """
+    Add scatter plots to HDF5 serialization of
+    constellation plot.
+
+    Parameters
+    ----------
+    cell_set:
+        the CellSet containing the per-cell metadata
+    embedding_coords:
+        the (n_cells, 2) numpy array of embedding coordinates
+        (rows must be in the same order as the cells in cell_set)
+    fov:
+        the FieldOfView defining the visualization
+    discrete_color_map:
+        the dict mapping [type_field][type_value] to color
+    color_by:
+        the type_field we are coloring by
+        (if None, everything will be gray)
+    hdf5_path:
+       path to hdf5 file where data is being stored
+    tmp_dir:
+        directory where we can write temporary png files
+    """
+    print("SERIALIZING SCATTERPLOTS")
+    tmp_dir = pathlib.Path(tmp_dir)
+    color_by_list = cell_set.type_field_list() + [None]
+    for color_by in color_by_list:
+        print(f"    SERIALIZED SCATTERPLOT COLORED BY {color_by}")
+        png_path = pathlib.Path(
+            mkstemp_clean(
+                dir=tmp_dir,
+                prefix=str(color_by)+'_',
+                suffix='.png'
+            )
+        )
+
+        try:
+            plot_embedding(
+                cell_set=cell_set,
+                embedding_coords=embedding_coords,
+                fov=fov,
+                discrete_color_map=discrete_color_map,
+                color_by=color_by,
+                dst_path=png_path
+            )
+            with h5py.File(hdf5_path, 'a') as dst:
+                with open(png_path, 'rb') as src:
+                    data = np.void(src.read())
+                dst.create_dataset(
+                    f'scatter_plots/{str(color_by)}',
+                    data=data
+                )
+        finally:
+            png_path.unlink()
