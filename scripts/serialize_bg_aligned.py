9fb6e28da6eb662cf40787b7bb66e4ea171aa3a2
danielsf <scott.daniel@alleninstitute.org>
2025-06-23 13:51:11 -0700

    use abc_atlas_access to serialize HMBA BG

--- scripts/serialize_bg_aligned.py
+++ scripts/serialize_bg_aligned.py
@@ -1,20 +1,59 @@
-import matplotlib.figure
-import numpy as np
+import argparse
 import pathlib
-import PIL.ImageColor
 
-import cell_type_constellations.serialization.ingestion as ingestion_utils
 import cell_type_constellations.serialization.serialization as serialization_utils
-import cell_type_constellations.plotting.plotting_api as plotting_api
+import abc_atlas_access.abc_atlas_cache.abc_project_cache as cache_module
 
 
 def main():
-    version = '20250507'
-    dst_h5_path = pathlib.Path(f'../../app_data/bg_aligned.for_download.{version}.h5')
-    cell_to_cluster_path=f'data/{version}/cell_to_cluster_membership.csv'
-    cluster_membership_path=f'data/{version}/cluster_to_cluster_annotation_membership.csv'
-    cluster_annotation_path=f'data/{version}/cluster_annotation_term.csv'
-    embedding_path=f'data/{version}/cell_2d_embedding_coordinates.csv'
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--dst_path', type=str, default=None
+    )
+    parser.add_argument(
+       '--clobber', default=False, action='store_true'
+    )
+    args = parser.parse_args()
+
+    if args.dst_path is None:
+        raise ValueError(
+            "Must specify dst_path"
+        )
+
+    abc_dir = pathlib.Path(
+        "/allen/programs/celltypes/workgroups/rnaseqanalysis/"
+        "lydian/ABC_handoff"
+    )
+
+    assert abc_dir.is_dir()
+
+    abc_cache = cache_module.AbcProjectCache.from_local_cache(
+        abc_dir
+    )
+
+    taxonomy_dir = 'HMBA-BG-taxonomy-CCN20250428'
+    aligned_dir = 'HMBA-10xMultiome-BG-Aligned'
+
+    cell_to_cluster_path = abc_cache.get_metadata_path(
+        directory=taxonomy_dir,
+        file_name='cell_to_cluster_membership'
+    )
+
+    cluster_membership_path = abc_cache.get_metadata_path(
+       directory=taxonomy_dir,
+       file_name='cluster_to_cluster_annotation_membership'
+    )
+
+    cluster_annotation_path = abc_cache.get_metadata_path(
+        directory=taxonomy_dir,
+        file_name='cluster_annotation_term'
+    )
+
+    embedding_path = abc_cache.get_metadata_path(
+        directory=taxonomy_dir,
+        file_name='cell_2d_embedding_coordinates'
+    )
+
     hierarchy=['Neighborhood', 'Class', 'Subclass', 'Group', 'Cluster']
 
     serialization_utils.serialize_from_csv(
@@ -23,8 +62,8 @@ def main():
             cluster_annotation_path=cluster_annotation_path,
             embedding_path=embedding_path,
             hierarchy=hierarchy,
-            dst_path=dst_h5_path,
-            clobber=True
+            dst_path=args.dst_path,
+            clobber=args.clobber
         )
 
 if __name__ == "__main__":
